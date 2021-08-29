0f8d078e7e52c6b0b7cdd1a2c916a29fbad00962
ranga <rangainlive@gmail.com>
2021-08-29 19:51:42 +0530

    Handling different routes

--- Lesson_04-Express.js/app.js
+++ Lesson_04-Express.js/app.js
@@ -4,15 +4,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+
+app.use("/add-product", (req, res, next) => {
   console.log("In the middleware");
-  next();
+  res.send("<h2>Add Product</h2>");
 });
-app.use((req, res, next) => {
+
+app.use("/", (req, res, next) => {
   console.log("In the another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
-
 // const server = http.createServer(app);
 
 // server.listen(5050);
