3ea84126962fa7160b0edef91ab2b879109fcae6
ranga <rangainlive@gmail.com>
2021-08-29 20:32:55 +0530

    parsing incoming request

--- Lesson_04-Express.js/app.js
+++ Lesson_04-Express.js/app.js
@@ -1,13 +1,21 @@
 // const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
   console.log("In the middleware");
-  res.send("<h2>Add Product</h2>");
+  res.send(
+    "<form action='/product' method='POST'><input name='title' type='text'/><button type='submit'>Add product</button></form>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
