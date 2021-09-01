2107d1f9cc19c0a7d8c2e978289fd26d94eff9c8
ranga <rangainlive@gmail.com>
2021-09-01 08:44:25 +0530

    Serving HTML pages

--- Lesson_04-Express.js/routes/admin.js
+++ Lesson_04-Express.js/routes/admin.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In the middleware");
-  res.send(
-    "<form action='/admin/add-product' method='POST'><input name='title' type='text'/><button type='submit'>Add product</button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
