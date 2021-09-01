9b5dea1a0ebe79cf1c4ace7f361ea3d1d1de3fdd
ranga <rangainlive@gmail.com>
2021-09-01 07:55:09 +0530

    Filtering path

--- Lesson_04-Express.js/routes/admin.js
+++ Lesson_04-Express.js/routes/admin.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In the middleware");
   res.send(
-    "<form action='/product' method='POST'><input name='title' type='text'/><button type='submit'>Add product</button></form>"
+    "<form action='/admin/add-product' method='POST'><input name='title' type='text'/><button type='submit'>Add product</button></form>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
